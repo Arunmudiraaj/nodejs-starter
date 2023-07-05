3b4d8d9d4df4a4b99d9b0b686942565f34e60e51
Arunmudiraaj <arunmudiraj18@gmail.com>
2023-07-05 11:24:21 +0530

    debugging

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const routes = require("./routes");
 const http = require("http");
-console.log(routes.text);
+console.log(routes.text + "h600h 999");
 const server = http.createServer(routes.requests);
 server.listen(3000);
